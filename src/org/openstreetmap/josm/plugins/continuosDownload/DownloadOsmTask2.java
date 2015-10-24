733c99db076e6cc86898a496c471a71d7b83338c
Vincent Privat <vincent.privat@gmail.com>
2015-10-24 22:08:25 +0200

    update to JOSM 8942

--- src/org/openstreetmap/josm/plugins/continuosDownload/DownloadOsmTask2.java
+++ src/org/openstreetmap/josm/plugins/continuosDownload/DownloadOsmTask2.java
@@ -1,51 +1,33 @@
 package org.openstreetmap.josm.plugins.continuosDownload;
 
-import static org.openstreetmap.josm.tools.I18n.tr;
-
 import java.util.concurrent.Future;
 
 import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
 import org.openstreetmap.josm.data.Bounds;
-import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.io.OsmServerReader;
 
-/*
+/**
  * This is a copy of the DownloadOsmTask that does not change the view after the area is downloaded.
  * It still displays modal windows and ugly dialog boxes :(
  */
 public class DownloadOsmTask2 extends DownloadOsmTask {
 
-    @Override
+    public DownloadOsmTask2() {
+    	warnAboutEmptyArea = false;
+	}
+
+	@Override
     public Future<?> download(OsmServerReader reader, boolean newLayer, Bounds downloadArea,
             ProgressMonitor progressMonitor) {
         return download(new DownloadTask2(newLayer, reader, progressMonitor), downloadArea);
-    }
-
-    @Override
-    protected void rememberErrorMessage(String message) {
-        // XXX: Remove error message that is not really an error to prevent ugly
-        // popups
-        if (!message.equals(tr("No data found in this area.")))
-            super.rememberErrorMessage(message);
     }
 
     protected class DownloadTask2 extends DownloadTask {
 
         public DownloadTask2(boolean newLayer, OsmServerReader reader,
                 ProgressMonitor progressMonitor) {
-            super(newLayer, reader, progressMonitor);
-        }
-
-        @Override
-        protected void computeBboxAndCenterScale(Bounds bounds) {
-            BoundingXYVisitor v = new BoundingXYVisitor();
-            if (bounds != null) {
-                v.visit(bounds);
-            } else {
-                v.computeBoundingBox(dataSet.getNodes());
-            }
-            // Main.map.mapView.recalculateCenterScale(v);
+            super(newLayer, reader, progressMonitor, false);
         }
     }
 }
